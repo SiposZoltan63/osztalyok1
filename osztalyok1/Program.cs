5cf4120534df1a5b1a1affcc0165af3ea71ca02e
SiposZoltan63 <siposz@kkszki.hu>
2025-09-24 07:59:49 +0200

    feladat1 is ready

---
+++ osztalyok1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osztalyok1
+{
+    public class butorlap
+    {
+        private int hosszusag;
+        private int szelesseg;
+        private string tipus;
+
+        public butorlap(int heigth, int width, string type)
+        {
+            heigth = hosszusag;
+            width = szelesseg;
+            type = tipus;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
